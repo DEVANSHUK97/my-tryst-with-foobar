72974c5d9253993140386655ff4c66c4208eddd8
DEVANSHUK97 <devanshu.khurma@mail.mcgill.ca>
2021-03-01 22:57:44 -0500

    solution works but is not optimised yet

--- Third/bomb, baby!.py
+++ Third/bomb, baby!.py
@@ -1,25 +1,31 @@
 def helper(x, y, m, f, steps):
+    print("-----------")
+    print(x,y,m,f,steps)
     if x == 1 and y == 1:
         return [0, True]
     if x == m+f and y == f:
         return [steps, True]
     if y == m+f and x == m:
         return [steps, True]
-    if x > m or y > f:
+    if x < m or y < f:
         return ["Impossible", False]
+
     choice1 = helper(x, y, m+f, f, steps+1)
     choice2 = helper(x, y, m, m+f, steps+1)
 
     if choice1[1] == False and choice2[1] == False:
         return ["Impossible", False]
-    elif choice1[1] == True and choice2[1] == True:
+    elif choice1[1] == True:
         return [choice1[0], True]
-    elif choice2[1] == True and choice1[1] == True:
-        return [choice2[0], True]
     else:
-        return [min(choice1[0], choice2[0]), True]
+        return [choice2[0], True]
 
 def solution(x, y):
     # Your code here
     ans = helper(x, y, 1, 1, 1)
-    return string(ans[0])
+    return str(ans[0])
+
+print(solution(1,2))
+print(solution(2,4))
+print(solution(3,1))
+print(solution(4,7))
